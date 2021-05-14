27b5622b892cd99dcc6e3809debf61060bd315d4
Tiago Murilo <tiago.murilo08@gmail.com>
2021-05-14 11:03:11 -0300

    adjust resume

--- pages/resume.js
+++ pages/resume.js
@@ -3,8 +3,9 @@ import style from '../components/resume.module.css'
 function Resume(){
     return(
         <div className={style.body}>
-            <h1>Pagina em Desenvolvimento.</h1>
-            <h3>Em breve será atualizada</h3>
+            <h1>Currículo</h1>
+            <br />
+            <iframe src="https://github.com/tiagomol1/curriculo/blob/master/TiagoMurilo.pdf" width="100%"></iframe>
         </div>
     )
 }
